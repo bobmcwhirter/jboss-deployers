aa9602a7c45609e198c274ed936f64fb8aa9c239
alesj <alesj@84be2c1e-ba19-0410-b317-a758671a6fc1>
2008-07-22 11:31:17 +0000

    Add integration deployers, non-abstract part - TODO on tests.
    git-svn-id: http://anonsvn.jboss.org/repos/jbossas/projects/jboss-deployers/trunk@76097 84be2c1e-ba19-0410-b317-a758671a6fc1

---
+++ deployers-vfs/src/main/org/jboss/deployers/vfs/plugins/classloader/FilesUrlIntegrationDeployer.java
@@ -0,0 +1,58 @@
+/*
+* JBoss, Home of Professional Open Source
+* Copyright 2006, JBoss Inc., and individual contributors as indicated
+* by the @authors tag. See the copyright.txt in the distribution for a
+* full listing of individual contributors.
+*
+* This is free software; you can redistribute it and/or modify it
+* under the terms of the GNU Lesser General Public License as
+* published by the Free Software Foundation; either version 2.1 of
+* the License, or (at your option) any later version.
+*
+* This software is distributed in the hope that it will be useful,
+* but WITHOUT ANY WARRANTY; without even the implied warranty of
+* MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+* Lesser General Public License for more details.
+*
+* You should have received a copy of the GNU Lesser General Public
+* License along with this software; if not, write to the Free
+* Software Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA
+* 02110-1301 USA, or see the FSF site: http://www.fsf.org.
+*/
+package org.jboss.deployers.vfs.plugins.classloader;
+
+/**
+ * Checks files if matching integration deployment.
+ *
+ * @param <T> exact output type
+ * @author <a href="ales.justin@jboss.com">Ales Justin</a>
+ */
+public abstract class FilesUrlIntegrationDeployer<T> extends UrlIntegrationDeployer<T>
+{
+   private String[] files;
+
+   protected FilesUrlIntegrationDeployer(Class<T> input)
+   {
+      super(input);
+   }
+
+   /**
+    * Get the matching files.
+    *
+    * @return the files
+    */
+   public String[] getFiles()
+   {
+      return files;
+   }
+
+   /**
+    * Set the matching files.
+    *
+    * @param files the files
+    */
+   public void setFiles(String[] files)
+   {
+      this.files = files;
+   }
+}
