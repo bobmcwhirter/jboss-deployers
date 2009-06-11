ff719532d1d16bc0a29c1dcc9653595502f43829
alesj <alesj@84be2c1e-ba19-0410-b317-a758671a6fc1>
2009-06-11 20:38:12 +0000

    Better compatibility check/info. Javadocs.
    git-svn-id: http://anonsvn.jboss.org/repos/jbossas/projects/jboss-deployers/trunk@90105 84be2c1e-ba19-0410-b317-a758671a6fc1

--- deployers-vfs-spi/src/main/java/org/jboss/deployers/vfs/spi/structure/modified/MetaDataStructureModificationChecker.java
+++ deployers-vfs-spi/src/main/java/org/jboss/deployers/vfs/spi/structure/modified/MetaDataStructureModificationChecker.java
@@ -36,6 +36,14 @@ import org.jboss.virtual.VirtualFileFilter;
 /**
  * MetaDataStructureModificationChecker.
  *
+ * VFS and structure cache filter must be compatible,
+ * meaning they would return same value for equal files/paths.
+ *
+ * e.g.
+ * VirtualFile file = VFS.getVirtualFile(rootURI, "/my-app.jar/META-INF/somefile.xml");
+ * String cachedPath = "/my-app.jar/META-INF/somefile.xml";
+ * filter.accepts(file) == cacheFilter.accepts(cachedPath)
+ *
  * @author <a href="mailto:ales.justin@jboss.org">Ales Justin</a>
  */
 public class MetaDataStructureModificationChecker extends AbstractStructureModificationChecker<Long>
@@ -79,9 +87,17 @@ public class MetaDataStructureModificationChecker extends AbstractStructureModif
     */
    public void start()
    {
-      if (cacheFilter == null && filter instanceof StructureCacheFilter)
+      if (cacheFilter == null)
       {
-         cacheFilter = (StructureCacheFilter) filter;
+         if (filter instanceof StructureCacheFilter)
+            cacheFilter = (StructureCacheFilter) filter;
+         else
+            log.warn("No cache filter specified, possible non match on leaves.");
+      }
+      else if (cacheFilter != filter)
+      {
+         // not the same instance
+         log.debug("VFS filter and structure cache filter are not the same instance, possible compatibility issue?");
       }
    }
 
